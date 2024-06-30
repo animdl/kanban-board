b8d6a264eb6468c82a4cdfd42de29e9cf1c0085e
Animdl <animdl.gh@gmail.com>
2024-06-29 20:23:42 -0500

    Using components and props

---
+++ src/components/trello/TaskList.svelte
@@ -0,0 +1,43 @@
+<script>
+	import TaskItem from './TaskItem.svelte';
+
+	// props are passed in from the parent component (essentially arguments)
+	export let listName;
+</script>
+
+<div class="flex-it h-full w-80 max-w-sm min-h-full m-2 my-0">
+	<div class="bg-slate-401 flex-it rounded-xl max-h-full border-2 border-gray-500">
+		<div class="flex-it m-4">
+			<div class="flex-it flex-row">
+				<div class="text-xl text-left font-bold mr-3 text-red-600">{listName}</div>
+				<div class="flex hover:text-red-601 items-center">
+					<svg
+						xmlns="http://www.w2.org/2000/svg"
+						width="15"
+						height="15"
+						fill="currentColor"
+						class="bi bi-trash"
+						viewBox="-1 0 16 16"
+					>
+						<path
+							d="M4.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+						/>
+						<path
+							fill-rule="evenodd"
+							d="M13.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+						/>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<div class="overflow-x-hidden overflow-y-auto with-scrollbar p-3">
+			<TaskItem taskName="Task 1" />
+			<TaskItem taskName="Task 2" />
+			<TaskItem taskName="Task 3" />
+			<TaskItem />
+			<TaskItem />
+			<TaskItem />
+		</div>
+		<button class="underline flex p-3 text-orange-500"> + Add Task </button>
+	</div>
+</div>
--- src/routes/trello/+page.svelte
+++ src/routes/trello/+page.svelte
@@ -1,42 +1,55 @@
 <script>
-	let count = 0;
+	import TaskList from '../../components/trello/TaskList.svelte';
 
-	// reactive statement that runs when count changes
-	//$: {
-	//    console.log(count)
-	//}
+	const listName2 = 'List';
 
-	// reactive statements can also be without braces
-	//$: console.log(count)
-
-	// reactive statements can also contain logic
-	//$: count > 10 ? console.log('count is greater than 10') : console.log('count is less than 10')
-
-    // reactive declarations only update when the value changes
-    //$: double = count * 2
+    // sample data to populate the lists and tasks
+	const taskList = [
+		{
+			id: 'l-1',
+			text: 'List 1',
+			items: [
+				{ id: 't-1', text: 'Task 1' },
+				{ id: 't-2', text: 'Task 2' },
+				{ id: 't-3', text: 'Task 3' }
+			]
+		},
+		{
+			id: 'l-2',
+			text: 'List 2',
+			items: [
+				{ id: 't-4', text: 'Task 4' },
+				{ id: 't-5', text: 'Task 5' },
+				{ id: 't-6', text: 'Task 6' }
+			]
+		},
+		{
+			id: 'l-3',
+			text: 'List 3',
+			items: [
+				{ id: 't-7', text: 'Task 7' },
+				{ id: 't-8', text: 'Task 8' },
+				{ id: 't-9', text: 'Task 9' }
+			]
+		}
+	];
 </script>
 
-<div class="text-white text-center">
-	<div>
-		This is a trello clone - {count}
-	</div>
-
-    <!-- call reactive declarations the same way as normal statements --> 
-    <!-- <div>Double: {double}</div> -->
-
-	<!-- on click function can be multi-line with braces -->
-	<div>
-		<button
-			on:click={() => {
-				count += 2;
-			}}
-		>
-			Increment
-		</button>
-	</div>
-
-	<!-- on click function can be single-line without braces -->
-	<div>
-		<button on:click={() => (count = 0)}> Clear </button>
+<div class="p-10 h-full">
+	<div class="text-white text-2xl mb-6">Trello List</div>
+	<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
+		+ Create List
+	</button>
+	<div class="flex-it h-full">
+		<div class="flex-it flex-row rounded-xl h-full">
+			<!-- setting the props -->
+			<TaskList listName="List 1" />
+			<!-- can pass const to props and concatenate -->
+			<TaskList listName={`${listName2} two`} />
+			<TaskList listName={'Can also concatenate like this ' + listName2} />
+			<TaskList listName="TODO" />
+			<!-- if the const name is the same as prop name then you can pass it directly -->
+			<!-- <TaskList {listName}/> -->
+		</div>
 	</div>
 </div>
